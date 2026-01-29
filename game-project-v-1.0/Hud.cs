34189bea5f80dc06380d606c03b5940edea479d2
Maskmaster64 <fai21013@byui.edu>
2026-01-29 08:48:35 -0700

    Completed a basic start menu

---
+++ game-project-v-1.0/Hud.cs
@@ -0,0 +1,24 @@
+using Godot;
+using System;
+
+public partial class Hud : CanvasLayer
+{
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+	}
+
+	private void _on_button_pressed()
+	{
+		// Logic to switch to the game scene
+		if (GameScene != null)
+		{
+			GetTree().ChangeSceneToPacked(GameScene); // Change the current scene to the game scene
+	 	}
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+}
