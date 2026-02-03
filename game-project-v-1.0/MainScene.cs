1fdeee3d5aedd8d920a14dda2847ccdf1b0ef351
Maskmaster64 <fai21013@byui.edu>
2026-02-03 09:08:10 -0700

    Integrating Hud into Tile map

--- game-project-v-1.0/MainScene.cs
+++ game-project-v-1.0/MainScene.cs
@@ -1,24 +1,24 @@
 using Godot;
 using System;
 
-public partial class Hud : CanvasLayer
+public partial class MainScene : Node2D
 {
-	[Signal]
-	public delegate void StartGameEventHandler();
-	
-	
+	private TileMap _tileMap;
+	private Hud _hud;
 	
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		_tileMap = GetNode<TileMap>("Stage");
+		_hud = GetNode<Hud>("HUD");
+		
+		_hud.StartGame += StartStage;
 	}
 
-	private void _on_button_pressed()
-	{
-		Visible = false;
-		EmitSignal(SignalName.StartGame);
+	private void StartStage(){
+		_tileMap.Visible = true;
 	}
-
+	
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
