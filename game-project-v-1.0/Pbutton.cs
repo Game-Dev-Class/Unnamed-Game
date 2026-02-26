18d8ff989b3b66fb5281b29114bb80c167dc8d26
Maskmaster64 <fai21013@byui.edu>
2026-02-26 09:25:31 -0700

    removed unneeded code

--- game-project-v-1.0/Pbutton.cs
+++ game-project-v-1.0/Pbutton.cs
@@ -4,14 +4,10 @@ using System;
 public partial class Pbutton : Area2D
 {
 	private Area2D _area2D;
-	//[Signal]
-	//public delegate void ButtonPressedEventHandler();
-	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
 		_area2D = GetNode<Area2D>("Area2D");
 		_area2D.BodyEntered += OnBodyEntered;
-		//Connect("body_entered", OnBodyEntered(Node2D body));
 		Monitoring = true;
 	}
 	
@@ -21,7 +17,6 @@ public partial class Pbutton : Area2D
 		{
 			GD.Print("Player has pressed the button");
 		}
-		//EmitSignal(SignalName.ButtonPressed);
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
