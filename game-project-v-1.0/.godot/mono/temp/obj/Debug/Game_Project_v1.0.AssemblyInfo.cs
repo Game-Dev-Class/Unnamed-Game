ed4078ab26bb92e82a46e709775816bbe3f0815c
Maskmaster64 <fai21013@byui.edu>
2026-03-17 08:44:57 -0600

    put the door back into the Main scene

--- game-project-v-1.0/.godot/mono/temp/obj/Debug/Game_Project_v1.0.AssemblyInfo.cs
+++ game-project-v-1.0/.godot/mono/temp/obj/Debug/Game_Project_v1.0.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Game_Project_v1.0")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+201d73e143b38d1e225f03ca5ff34677ca58bb0b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9f1511b905c968a3d5c9316a411d1c029851a7e0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Game_Project_v1.0")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Game_Project_v1.0")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
