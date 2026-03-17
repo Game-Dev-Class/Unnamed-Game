3a7932bf7502b5d788a92e992f3520a85bc52a50
colterjorgensen4-ux <colterjorgensen4@gmail.com>
2026-03-17 08:01:43 -0600

    fixed level exit

--- game-project-v-1.0/.godot/mono/temp/obj/Debug/Game_Project_v1.0.AssemblyInfo.cs
+++ game-project-v-1.0/.godot/mono/temp/obj/Debug/Game_Project_v1.0.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Game_Project_v1.0")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+60f3ee72dc137d3961b3472179df863b6b75eb38")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f2c4c067cd5b63f1ce3683a513fe6b2660a5d643")]
 [assembly: System.Reflection.AssemblyProductAttribute("Game_Project_v1.0")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Game_Project_v1.0")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
