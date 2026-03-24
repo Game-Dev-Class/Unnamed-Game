bf773f0c395fee4162809b6a142995d279b8b12f
Maskmaster64 <fai21013@byui.edu>
2026-03-24 09:11:47 -0600

    fixed vscode debugger

--- game-project-v-1.0/.godot/mono/temp/obj/Debug/Game_Project_v1.0.AssemblyInfo.cs
+++ game-project-v-1.0/.godot/mono/temp/obj/Debug/Game_Project_v1.0.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Game_Project_v1.0")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+96fa7ad11943ca77f02a2ab937634040f61b6f2b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2cc3f0e28c8c9a0cb18bfc75b3789ba3d31c9e4b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Game_Project_v1.0")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Game_Project_v1.0")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
