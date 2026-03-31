c06473e01cd898fa8fdca5a96abbc88524feb173
colterjorgensen4-ux <colterjorgensen4@gmail.com>
2026-03-31 17:21:58 -0600

    added music, sprites, sound, and got rid of greyspace

--- game-project-v-1.0/.godot/mono/temp/obj/Debug/Game_Project_v1.0.AssemblyInfo.cs
+++ game-project-v-1.0/.godot/mono/temp/obj/Debug/Game_Project_v1.0.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Game_Project_v1.0")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+535f4c034f98f1fb475b30178661d508d2c540ad")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+acc67bc4be616c47505fd85812a264de6928a073")]
 [assembly: System.Reflection.AssemblyProductAttribute("Game_Project_v1.0")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Game_Project_v1.0")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- game-project-v-1.0/Scripts/Pbutton.cs
+++ game-project-v-1.0/Scripts/Pbutton.cs
@@ -6,24 +6,39 @@ public partial class Pbutton : Area2D
 	[Signal]
 	public delegate void ButtonTriggerEventHandler();
 
+	[Export] public AudioStreamPlayer2D ClickSound; // Exported for the Inspector
+
 	private Area2D _area2D;
+
 	public override void _Ready()
 	{
-		_area2D = GetNode<Area2D>("Area2D");
-		_area2D.BodyEntered += OnBodyEntered;
+		// Fallback: If you didn't drag it in the inspector, try to find a child named "ClickSound"
+		ClickSound ??= GetNodeOrNull<AudioStreamPlayer2D>("ClickSound");
+
+		_area2D = GetNodeOrNull<Area2D>("Area2D");
+		
+		if (_area2D != null)
+		{
+			_area2D.BodyEntered += OnBodyEntered;
+		}
+
 		Monitoring = true;
 	}
 	
 	public void OnBodyEntered(Node2D body)
 	{
-		if(body is CharacterBody2D character)
+		// Check for Player specifically (or any CharacterBody2D like your enemies)
+		if (body is CharacterBody2D character)
 		{
 			GD.Print("Player has pressed the button");
+			
+			// The ?. ensures it only plays if the node exists
+			ClickSound?.Play();
+			
 			EmitSignal(SignalName.ButtonTrigger);
 		}
 	}
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
 	}
