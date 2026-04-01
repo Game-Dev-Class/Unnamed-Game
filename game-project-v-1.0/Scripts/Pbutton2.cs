f64b67baf1656d3b64138b99b74588b12987ff37
Narwhal <158131086+narwhal027@users.noreply.github.com>
2026-03-31 22:20:35 -0600

    Fixed button sound in final level

--- game-project-v-1.0/Scripts/Pbutton2.cs
+++ game-project-v-1.0/Scripts/Pbutton2.cs
@@ -6,19 +6,29 @@ public partial class Pbutton2 : Area2D
     [Signal]
     public delegate void Button2TriggerEventHandler();
 
+    [Export] public AudioStreamPlayer2D ClickSound;
+
     private Area2D _area2D;
+
     public override void _Ready()
     {
-        _area2D = GetNode<Area2D>("Area2D");
-        _area2D.BodyEntered += OnBodyEntered;
+        ClickSound ??= GetNodeOrNull<AudioStreamPlayer2D>("ClickSound");
+        _area2D = GetNodeOrNull<Area2D>("Area2D");
+
+        if (_area2D != null)
+            _area2D.BodyEntered += OnBodyEntered;
+
         Monitoring = true;
     }
 
     public void OnBodyEntered(Node2D body)
     {
-        if (body is CharacterBody2D character)
+        if (body is CharacterBody2D)
         {
             GD.Print("Player has pressed the button for PDoor2");
+
+            ClickSound?.Play();
+
             EmitSignal(SignalName.Button2Trigger);
         }
     }
